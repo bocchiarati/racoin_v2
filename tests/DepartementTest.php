d36b7ad4059bcb9223a99e00d4da2812ebf37087
suki <88000sofien@gmail.com>
2026-03-24 16:39:13 +0100

    Create tests files

---
+++ tests/DepartementTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DepartementTest extends TestCase
+{
+    function testGetAllDepartements()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/ItemTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ItemTest extends TestCase
+{
+    function testIsEmail()
+    {
+        $this->assertTrue(true);
+    }
+
+    function testAddNewItem()
+    {
+        $this->assertTrue(true);
+    }
+
+    function testSupprimerItem()
+    {
+        $this->assertTrue(true);
+    }
+}
